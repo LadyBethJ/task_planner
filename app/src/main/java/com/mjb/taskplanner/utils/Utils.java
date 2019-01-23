fa981c389395bb72ce0b695a4b43c72799306f70
Maribel Jurado <maribel.juradobaena@gmail.com>
2019-01-23 19:44:34 +0100

    created class utls

---
+++ app/src/main/java/com/mjb/taskplanner/utils/Utils.java
@@ -0,0 +1,8 @@
+package com.mjb.taskplanner.utils;
+
+public class Utils {
+
+    public static boolean isNotNullNorEmpty(String string){
+        return !string.isEmpty() && string.length()>0;
+    }
+}
